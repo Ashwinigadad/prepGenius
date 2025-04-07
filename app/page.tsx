0d8448de8bcbe3684c5bf9493d9a3fc82a3b56f4
Ashwinigadad <ashwinigadad3@gmail.com>
2025-04-07 18:15:50 +0530

    logo

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>hi
-      <br />
-      <Button>Button</Button>
-<h1>hello world</h1>
+
     </div>
 
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+'use client';
+
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <header className="flex items-center justify-between px-4 py-3 border-b border-border">
+      <nav className="flex items-center gap-4">
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            width={160}
+            height={60}
+            className="rounded"
+          />
+        </Link>
+      </nav>
 
-export default Header
+      <div className="flex items-center gap-4">
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton />
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
