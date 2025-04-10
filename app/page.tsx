54ab08acf31303d2875b9b829048f1d55c9b3598
Ashwinigadad <ashwinigadad3@gmail.com>
2025-04-10 19:13:16 +0530

    features section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
 "use client"
 import HeroSection from "@/components/hero";
 import { Card, CardContent } from "@/components/ui/card";
-import { features } from "process";
+import { features } from "@/data/features";
+import { motion } from "framer-motion";
+
+import { testimonial } from "@/data/testimonial";
+import { faqs } from "@/data/faqs";
+import { howItWorks } from "@/data/howItWorks";
 
 
 export default function page() {
+
+  const cardVariants = {
+    hidden: { opacity: 0, y: 40 },
+    visible: (i: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: i * 0.2,
+        duration: 0.6,
+        ease: "easeOut",
+      },
+    }),
+  };
   return (
     <div>
       <div className="grid-background">
@@ -12,26 +30,34 @@ export default function page() {
 
             {/* Hero Section */}
             <HeroSection />
-
-            <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
+{/* features */}
+<section className="w-full py-12 md:py-24 lg:py-32 bg-background">
       <div className="container mx-auto px-4 md:px-6">
         <h2 className="text-3xl font-bold tracking-tighter text-center mb-12">
           Powerful Features for Your Career Growth
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
           {features.map((feature, index) => (
-            <Card
+            <motion.div
               key={index}
-              className="border-2 hover:border-primary transition-colors duration-300"
+              custom={index}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.3 }}
+              variants={cardVariants}
             >
-              <CardContent className="pt-6 text-center flex flex-col items-center">
-                <div className="flex flex-col items-center justify-center">
-                  {feature.icon}
-                  <h3 className="text-xl font-bold mb-2">{feature.title}</h3>
-                  <p className="text-muted-foreground">{feature.description}</p>
-                </div>
-              </CardContent>
-            </Card>
+              <Card className="border-2 hover:border-primary transition-colors duration-300">
+                <CardContent className="pt-6 text-center flex flex-col items-center">
+                  <div className="flex flex-col items-center justify-center">
+                    {feature.icon}
+                    <h3 className="text-xl font-bold mb-2">{feature.title}</h3>
+                    <p className="text-muted-foreground">
+                      {feature.description}
+                    </p>
+                  </div>
+                </CardContent>
+              </Card>
+            </motion.div>
           ))}
         </div>
       </div>
